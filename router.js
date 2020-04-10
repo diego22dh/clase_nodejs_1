d3bd7ccca803cbdcee25a9f88179b052dcf2c4a6
diego22dh <diegoratti45@hotmail.com>
2020-04-10 10:41:40 -0300

    modificacion nombre de funciones de las distintas paginas

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const funciones = require("./customFunctions.js");
-const otrasFunciones = require ("./src/index")
+const funcionesPaginas = require ("./src/index")
 
 
 const route = (req,res)=>{
@@ -8,23 +8,23 @@ const route = (req,res)=>{
      switch (req.url) {
        // Home
        case "/":
-         otrasFunciones.home.homePage(req, res);
+         funcionesPaginas.home.homePage(req, res);
          break;
        // En cartelera
        case "/en-cartelera":
-         otrasFunciones.cartelera.enCartelera(req, res);
+         funcionesPaginas.cartelera.enCartelera(req, res);
          break;
        case "/mas-votadas":
-         otrasFunciones.masVotadas.masVotadas(req, res);
+         funcionesPaginas.masVotadas.masVotadas(req, res);
          break;
        case "/sucursales":
-         otrasFunciones.sucursales.sucursales(req, res);
+         funcionesPaginas.sucursales.sucursales(req, res);
          break;
        case "/contacto":
-         otrasFunciones.contacto.contacto(req, res);
+         funcionesPaginas.contacto.contacto(req, res);
          break;
        case "/preguntas-frecuentes":
-         otrasFunciones.faqs.faqs(req, res);
+         funcionesPaginas.faqs.faqs(req, res);
          break;
        default:
          res.end("404 not found");
